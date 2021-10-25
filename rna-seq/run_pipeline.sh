82edf114fa23f019eb5b79c170c441e604c332b5
Taishan Li <61141659+707728642li@users.noreply.github.com>
2021-10-25 21:26:16 +0900

    Update run_pipeline.sh

--- rna-seq/run_pipeline.sh
+++ rna-seq/run_pipeline.sh
@@ -4,19 +4,24 @@ job_num=1
 thread_num=8
 
 #input and out put directories
-fastq_dir="./fastq"
 fastq_suffix="fastq.gz"
+
+fastq_dir="./fastq"
 clean_fastq_dir="./clean_fastq"
 sam_dir="./sam_folder"
 bam_dir="./bam_folder"
-genome_dir="./genome/genome"
+hisat2_dir="./histat2_dir"
+
 gff_file="./genome/genome.gff"
+genome_fa="./genome/genome.fa"
 
 for fd in fastq_dir clean_fastq_dir sam_dir bam_dir genome_dir ; do
 [ -a ${fd} ] || mkdir ${fd}
 done
 
-#sample_id
+#build hisat2 index
+
+# get all sample_id
 [ -a sample_id.txt ] || ls ${fastq_dir} | sed "s/[12].${fastq_suffix}//g" | sort | uniq > sample_id.txt
 echo =============================================================  
 echo $(cat sample_id.txt | wc -l) samples are found in sample_id.txt:
