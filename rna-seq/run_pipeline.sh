af92db811dcf2bfd606dc76af400c7221914c170
LI TAISHAN <litaishan910706@gmail.com>
2021-10-25 22:18:14 +0900

    beta

--- rna-seq/run_pipeline.sh
+++ rna-seq/run_pipeline.sh
@@ -20,6 +20,8 @@ for fd in fastq_dir clean_fastq_dir sam_dir bam_dir genome_dir ; do
 done
 
 #build hisat2 index
+[ -a ${hisat2_dir}/genome.1.ht2 ] || hisat2-build -p ${thread_num} ${genome_fa} ${hisat2_dir}/genome
+
 
 # get all sample_id
 [ -a sample_id.txt ] || ls ${fastq_dir} | sed "s/[12].${fastq_suffix}//g" | sort | uniq > sample_id.txt
@@ -28,6 +30,7 @@ echo $(cat sample_id.txt | wc -l) samples are found in sample_id.txt:
 cat -n sample_id.txt
 echo =============================================================
 
+# Run all tasks with parallel function
 cat sample_id.txt | parallel -j ${job_num} \
 bash run_each.sh ${sample_id}\ #$1
 				 ${fastq_suffix}\ #$2
@@ -38,10 +41,18 @@ bash run_each.sh ${sample_id}\ #$1
 				 ${thread_num}\ #$7
 				 ${genome_dir}\ #$8
 
+# get mapping infomation
+for bam in `ls ${bam_dir}` ; do
+	echo ===== ${bam} ===== >> mapping.summary.txt
+	samtools flagstat ${bam} >> mapping.summary.txt
+	echo >> mapping.summary.txt 
+done
+
+# calculate the counts
 featureCounts -B \
 			  -T 8 \
 			  -t exon \
-			  -g Parent \
+			  -g Parent \ #if gtf file was provided, change Parent to gene_id
 			  -a ${gff_file} \
 			  -o result.featureCounts.txt $(find ${bam_dir} -name *.bam)
 
