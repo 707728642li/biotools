5c533a3d8e95b640a3884b6a2bf3bbc6b372ed4e
Taishan Li <61141659+707728642li@users.noreply.github.com>
2021-10-27 14:42:20 +0900

    Update run_pipeline.sh

--- rna-seq/run_pipeline.sh
+++ rna-seq/run_pipeline.sh
@@ -32,14 +32,7 @@ log_dir="./logs"
 # ===================================================================
 
 # ======================= Preparation work ==========================
-
-if ! [ $1 = "-y" ]; then
-    # Get all sample_id
-    ls ${fastq_dir} | sed "s/_[12].${fastq_suffix}//g" | sort | uniq > sample_id.txt
-        echo =============================================================  
-        echo $(cat sample_id.txt | wc -l) samples are found in sample_id.txt:
-        cat -n sample_id.txt
-        echo -e "\033[31;47m Please check the list in ./sample_id.list and run bash run_pipline.sh -y to run the whole pipline! \033[0m"
+if ! [ $1 = "-y" ] ; then
     
     # Create result folders
     for fd in ${log_dir} ${fastq_dir} ${clean_fastq_dir} ${sam_dir} ${bam_dir} ${hisat2_dir_name%/*} ; do
@@ -47,13 +40,29 @@ if ! [ $1 = "-y" ]; then
     done
 
     # Build hisat2 index
-    [ -a ${hisat2_dir_name}.1.ht2 ] && \
-    ( echo Found hisat2 index under ${hisat2_dir_name%/*} ) || \
-    ( hisat2-build -p ${thread_num} ${genome_fa} ${hisat2_dir_name} && \
-    echo Build hisat2 index successfully! )
+    if [ -a ${hisat2_dir_name}.1.ht2 ] ; then
+        echo Found hisat2 index under ${hisat2_dir_name%/*}
+    else
+        echo Building hisat2 index ......
+        hisat2-build -q -p ${thread_num} ${genome_fa} ${hisat2_dir_name} 
+        echo Build hisat2 index successfully!
+    fi
+    
+    # Get all sample_id
+    ls ${fastq_dir} | sed "s/_[12].${fastq_suffix}//g" | sort | uniq > sample_id.txt
+    if [ $( cat sample_id.txt | wc -l ) -eq 0 ] ; then
+        echo No _1 or _2.${fastq_suffix} files was found under ${fastq_dir}, please check the file name or path!
+        exit 0
+    else
+        echo =============================================================  
+        echo $(cat sample_id.txt | wc -l) samples are found in sample_id.txt:
+        cat -n sample_id.txt
+        echo Please check the items in ./sample_id.list and run bash run_pipline.sh -y to run the whole pipline!
+    fi
 
     exit 0
 fi
+# ====================================================================
 
 if ! [ -a sample_id.txt ] ; then
     echo File: sample_id.txt was not found under ./ 
@@ -61,7 +70,7 @@ if ! [ -a sample_id.txt ] ; then
     exit 0 
 fi
 
-# Run all tasks with parallel function
+# =========================== Run all tasks ===========================
 cat sample_id.txt | parallel -j ${job_num} --bar bash run_each.sh {} \
                                                             ${fastq_suffix} \
                                                             ${fastq_dir} \
@@ -71,19 +80,19 @@ cat sample_id.txt | parallel -j ${job_num} --bar bash run_each.sh {} \
                                                             ${thread_num} \
                                                             ${hisat2_dir_name}
 
-# get mapping infomation
+# Get mapping infomation
 for bam in `ls ${bam_dir}/*bam` ; do
     echo ===== ${bam} ===== >> mapping.summary.txt
     samtools flagstat ${bam} >> mapping.summary.txt 2> /dev/null
     echo >> mapping.summary.txt 
 done
 
-# calculate the counts
-#if gtf file was provided, change Parent to gene_id for -g
+# Calculate the counts
+# If gtf file was provided, change Parent to gene_id for -g argument
 featureCounts -B \
               -T ${thread_num} \
               -t exon \
               -g Parent \
               -a ${gff_file} \
               -o result.featureCounts.txt $(find ${bam_dir} -name *.bam) &>> ./logs/fetureCounts.log
-
+# =====================================================================
