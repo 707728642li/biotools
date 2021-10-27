7cc51db27bee74ce2515a30b98416e126043c6d4
Taishan Li <61141659+707728642li@users.noreply.github.com>
2021-10-27 13:56:47 +0900

    Update run_pipeline.sh

--- rna-seq/run_pipeline.sh
+++ rna-seq/run_pipeline.sh
@@ -12,13 +12,16 @@ if [ $1 = "-h" ] || [ $1 = "--help" ] ; then
     exit 1 
 fi
 
+# ===================== Configuration Items ========================  
+# work performance
 job_num=8
-
 thread_num=8
 
-#input and output directories
+# Input directories
 fastq_suffix="fastq"
 fastq_dir="./test_fastq"
+
+# Output directories
 clean_fastq_dir="./clean_fastq"
 sam_dir="./sam_folder"
 bam_dir="./bam_folder"
@@ -26,14 +29,29 @@ hisat2_dir_name="./hisat2_dir/genome"
 gff_file="./genome/genome.gff"
 genome_fa="./genome/genome.fa"
 log_dir="./logs"
+# ===================================================================
 
-# get all sample_id
+# ======================= Preparation work ==========================
+
 if ! [ $1 = "-y" ]; then
-ls ${fastq_dir} | sed "s/_[12].${fastq_suffix}//g" | sort | uniq > sample_id.txt
-    echo =============================================================  
-    echo $(cat sample_id.txt | wc -l) samples are found in sample_id.txt:
-    cat -n sample_id.txt
-    echo -e "\033[31;47m Please check the list in ./sample_id.list and run bash run_pipline.sh -y to run the whole pipline! \033[0m"
+    # Get all sample_id
+    ls ${fastq_dir} | sed "s/_[12].${fastq_suffix}//g" | sort | uniq > sample_id.txt
+        echo =============================================================  
+        echo $(cat sample_id.txt | wc -l) samples are found in sample_id.txt:
+        cat -n sample_id.txt
+        echo -e "\033[31;47m Please check the list in ./sample_id.list and run bash run_pipline.sh -y to run the whole pipline! \033[0m"
+    
+    # Create result folders
+    for fd in ${log_dir} ${fastq_dir} ${clean_fastq_dir} ${sam_dir} ${bam_dir} ${hisat2_dir_name%/*} ; do
+        [ -d ${fd} ] || mkdir ${fd}
+    done
+
+    # Build hisat2 index
+    [ -a ${hisat2_dir_name}.1.ht2 ] && \
+    ( echo Found hisat2 index under ${hisat2_dir_name%/*} ) || \
+    ( hisat2-build -p ${thread_num} ${genome_fa} ${hisat2_dir_name} && \
+    echo Build hisat2 index successfully! )
+
     exit 0
 fi
 
@@ -42,16 +60,6 @@ if ! [ -a sample_id.txt ] ; then
     echo -e "\033[31;47m Please run bash run_pipline.sh to get the sample_id.txt! \033[0m"
     exit 0 
 fi
-
-for fd in ${log_dir} ${fastq_dir} ${clean_fastq_dir} ${sam_dir} ${bam_dir} ${hisat2_dir_name%/*} ; do
-    [ -d ${fd} ] || mkdir ${fd}
-done
-
-#build hisat2 index
-[ -a ${hisat2_dir_name}.1.ht2 ] && \
-( echo Found hisat2 index under ${hisat2_dir_name%/*} ) || \
-( hisat2-build -p ${thread_num} ${genome_fa} ${hisat2_dir_name} && \
-echo Build hisat2 index successfully! )
 
 # Run all tasks with parallel function
 cat sample_id.txt | parallel -j ${job_num} --bar bash run_each.sh {} \
